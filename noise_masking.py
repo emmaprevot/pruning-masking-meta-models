fixed pruning and masking and added tests

noise_masking.py
@@ -131,27 +131,33 @@ In this example, we're creating a binary mask for each layer of weights, where v
 if the corresponding weight is greater than the pruning threshold and 0 otherwise. 
 """
 
+
 def create_mask(params, prune_ratio):
     mask = {}
-    for layer_name, layer in params.items():
-        mask[layer_name] = {}
-        if 'w' in layer_name:
-            # Compute the threshold
-            threshold = jnp.percentile(jnp.abs(layer), prune_ratio * 100)
-
-            # Create a mask that is 1 where weights are greater than the threshold and 0 otherwise
-            mask[layer_name] = jnp.where(jnp.abs(layer) > threshold, 1, 0)
-
+    for layer, param_dict in params.items():
+        sub_mask = {}
+        for k, v in param_dict.items():
+            if 'w' in k:
+                # Compute the threshold
+                threshold = jnp.percentile(jnp.abs(v), prune_ratio * 100)
+                # Create a mask that is 1 where weights are greater than the threshold and 0 otherwise
+                sub_mask[k] = jnp.where(jnp.abs(v) > threshold, 1, 0)
+            else:
+                # For biases just create a mask of ones
+                sub_mask[k] = jnp.ones_like(v)
+        mask[layer] = sub_mask
     return mask
 
 def apply_mask(params, mask):
     pruned_params = {}
-    for layer_name, layer in params.items():
-        pruned_params[layer_name] = {}
-        if 'w' in layer_name:
-            pruned_params[layer_name] = layer * mask[layer_name]
-        else:
-            pruned_params[layer_name] = layer
+    for layer, param_dict in params.items():
+        layer_params = {}
+        for k, v in param_dict.items():
+            if 'w' in layer_name:
+                layer_param[k] = v * mask[layer][k]
+            else:
+                layer_param[k] = v
+        pruned_params[layer] = layer_param
     return pruned_params
 
 
@@ -186,29 +192,36 @@ def add_noise(params, noise_type = 'normal', std_dev=0.05, noise_ratio=0.1, scal
     Poisson noise is integer-valued and thus may not be suitable for all tasks.
     
     """
+    rng = jax.random.PRNGKey(0)
     params_noisy = {}
+    
+    for layer, param_dict in params.items():
+        layer_params = {}
+        for k, v in param_dict.items():
+            if 'w' in k:
+                weights = v
+                rng, subkey = jax.random.split(rng)
+                noise_mask = jax.random.bernoulli(subkey, noise_ratio, weights.shape)
+                rng, subkey = jax.random.split(rng)
 
-    for layer_name, layer_params in params.items():
-        if 'w' in layer_name:
-            weights = layer_params
-            noise_mask = jax.random.bernoulli(jax.random.PRNGKey(0), noise_ratio, weights.shape)
-            if noise_type == "normal":
-                noise_values = jax.random.normal(jax.random.PRNGKey(0), shape = weights.shape) * std_dev
-            elif noise_type == "uniform":
-                noise_values = jax.random.uniform(jax.random.PRNGKey(0), shape = weights.shape, minval=-scale, maxval=scale) 
-            elif noise_type == "laplace":
-                noise_values = jax.random.laplace(jax.random.PRNGKey(0), shape = weights.shape) * scale
-            elif noise_type == "bernoulli":
-                noise_values = jax.random.bernoulli(jax.random.PRNGKey(0), p=0.5, shape = weights.shape) * scale
-            elif noise_type == "cauchy":
-                noise_values = jax.random.cauchy(jax.random.PRNGKey(0), shape = weights.shape) * scale
-            elif noise_type == "poisson":
-                noise_values = jax.random.poisson(jax.random.PRNGKey(0), lam=lam, shape = weights.shape)
+                if noise_type == "normal":
+                    noise_values = jax.random.normal(subkey, shape = weights.shape) * std_dev
+                elif noise_type == "uniform":
+                    noise_values = jax.random.uniform(subkey, shape = weights.shape, minval=-scale, maxval=scale) 
+                elif noise_type == "laplace":
+                    noise_values = jax.random.laplace(subkey, shape = weights.shape) * scale
+                elif noise_type == "bernoulli":
+                    noise_values = jax.random.bernoulli(subkey, p=0.5, shape = weights.shape) * scale
+                elif noise_type == "cauchy":
+                    noise_values = jax.random.cauchy(subkey, shape = weights.shape) * scale
+                elif noise_type == "poisson":
+                    noise_values = jax.random.poisson(subkey, lam=lam, shape = weights.shape)
             
-            weights_noisy = jnp.where(noise_mask, weights + noise_values, weights)
-            params_noisy[layer_name] = weights_noisy
-        else:
-            params_noisy[layer_name] = layer
+                weights_noisy = jnp.where(noise_mask, weights + noise_values, weights)
+                layer_params[k] = weights_noisy
+            else:
+                layer_params[k] = v
+        params_noisy[layer] = layer_params
     return params_noisy
 
 

pruning.py
@@ -97,9 +97,10 @@ class CTCNet(hk.Module):
 
 
 # Load the hyperparameters from the JSON file
+# Here I am using the original path to weight zoo, to be changed depending on directory
 with open('./transfer/0/run_data.json', 'r') as f:
     data = json.load(f)
-hyperparameters = data['hyperparameters']
+hyperparameters = data['hyperparameters'] 
 
 
 def net_fn(x: jnp.ndarray, is_training: bool) -> jnp.ndarray:
@@ -116,6 +117,7 @@ def net_fn(x: jnp.ndarray, is_training: bool) -> jnp.ndarray:
 net = hk.transform(net_fn)
 
 params = jnp.load('./transfer/0/epoch_20.npy', allow_pickle=True).item()
+#print(params)
 
 print("NETWORK ARCHITECTURE")
 for layer, param_dict in params.items():
@@ -164,22 +166,28 @@ def prune_weights_smallest_layer(params, prune_ratio):
 
 def prune_weights_smallest_global(params, prune_ratio):
     # Concatenate all weights into a single 1D array
-    all_weights = jnp.concatenate([jnp.abs(v).flatten() for k, v in params.items() if 'w' in k])
+    all_weights = jnp.concatenate([jnp.abs(param_dict['w']).flatten() for layer, param_dict in params.items() if 'w' in param_dict])
+
     # Compute the pruning threshold
     threshold = jnp.percentile(all_weights, prune_ratio * 100)
+
     # Prune the weights
     pruned_params = {}
-    for k, v in params.items():
-        if 'w' in k:
-            pruned_params[k] = jnp.where(jnp.abs(v) < threshold, 0, v)
-        else:
-            pruned_params[k] = v
+    for layer, layer_param in params.items():
+        pruned_layer = {}
+        for k, v in layer_param.items():
+            if 'w' in k:
+                pruned_layer[k] = jnp.where(jnp.abs(v) < threshold, 0, v)
+            else:
+                pruned_layer[k] = v
+        pruned_params[layer] = pruned_layer
 
     return pruned_params
 
+#TESTED
 def snr_pruning_global(params, prune_ratio):
     # Compute the SNR for all weights
-    snr_values = {k: jnp.abs(v.mean()) / v.std() for k, v in params.items() if 'w' in k}
+    snr_values = {k: jnp.abs(param_dict['w'].mean()) / param_dict['w'].std() for k, param_dict in params.items() if 'w' in param_dict}
 
     # Flatten all SNR values into a single 1D array
     all_snr = jnp.concatenate([v.flatten() for v in snr_values.values()])
@@ -189,33 +197,41 @@ def snr_pruning_global(params, prune_ratio):
 
     # Prune the weights
     pruned_params = {}
-    for k, v in params.items():
-        if 'w' in k:
-            pruned_params[k] = jnp.where(snr_values[k] < threshold, 0, v)
-        else:
-            pruned_params[k] = v
+    for layer, layer_param in params.items():
+        pruned_layer = {}
+        for k, v in layer_param.items():
+            if 'w' in k:
+                snr_layer = snr_values[layer]
+                pruned_layer[k] = jnp.where(snr_layer < threshold, 0, v)
+            else:
+                pruned_layer[k] = v
+        pruned_params[layer] = pruned_layer
 
     return pruned_params
 
+
+#TESTED
 def snr_pruning_layer(params, prune_ratio):
     pruned_params = {}
-    for k, v in params.items():
-        if 'w' in k:
-            # Compute the SNR for the weights of this layer
-            snr_values = jnp.abs(v.mean()) / v.std()
+    for layer, param_dict in params.items():
+        layer_param = {}
+        for k, v in param_dict.items():
+            if 'w' in k:
+                # Compute the SNR for the weights of this layer
+                snr_values = jnp.abs(v) / v.std()
 
-            # Compute the pruning threshold for this layer
-            threshold = jnp.percentile(snr_values, prune_ratio * 100)
+                # Compute the pruning threshold for this layer
+                threshold = jnp.percentile(snr_values, prune_ratio * 100)
 
-            # Prune the weights of this layer
-            pruned_params[k] = jnp.where(snr_values < threshold, 0, v)
-        else:
-            pruned_params[k] = v
-
+                # Prune the weights of this layer
+                layer_param[k] = jnp.where(snr_values < threshold, 0, v)
+            else:
+                layer_param[k] = v
+        pruned_params[layer] = layer_param
     return pruned_params
 
 
-
+# TESTED
 def get_statistics(params):
     """ Instead of using the weights, we return 8 statistics for each layer (Classifying the classifier paper - feature-based meta-classification
 
@@ -226,9 +242,9 @@ def get_statistics(params):
     for layer_name, layer_params in params.items():
         stats_layer_params = {}
         for param_name, param_values in layer_params.items():
-            if param_name == 'w':  # only prune weights ('w')
+            if 'w' in param_name:  # only prune weights ('w')
                 stats = [np.mean(param_values), np.std(param_values)**2, skew(param_values), np.percentile(param_values, 1), 
-                 np.percentile(param_values, 25), np.percentile(param_values, 50), np.percentile(param_values, 75), np.percentile(param_values, 99)]
+                np.percentile(param_values, 25), np.percentile(param_values, 50), np.percentile(param_values, 75), np.percentile(param_values, 99)]
                 stats_layer_params[param_name] = stats
             else:
                 stats_layer_params[param_name] = param_values
@@ -247,20 +263,34 @@ In this example, we're creating a binary mask for each layer of weights, where v
 if the corresponding weight is greater than the pruning threshold and 0 otherwise. 
 """
 
+# TESTED
 def create_mask(params, prune_ratio):
     mask = {}
-    for k, v in params.items():
-        if 'w' in k:
-            # Compute the threshold
-            threshold = jnp.percentile(jnp.abs(v), prune_ratio * 100)
-
-            # Create a mask that is 1 where weights are greater than the threshold and 0 otherwise
-            mask[k] = jnp.where(jnp.abs(v) > threshold, 1, 0)
-        else:
-            # For biases just create a mask of ones
-            mask[k] = jnp.ones_like(v)
-
+    for layer, param_dict in params.items():
+        sub_mask = {}
+        for k, v in param_dict.items():
+            if 'w' in k:
+                # Compute the threshold
+                threshold = jnp.percentile(jnp.abs(v), prune_ratio * 100)
+                # Create a mask that is 1 where weights are greater than the threshold and 0 otherwise
+                sub_mask[k] = jnp.where(jnp.abs(v) > threshold, 1, 0)
+            else:
+                # For biases just create a mask of ones
+                sub_mask[k] = jnp.ones_like(v)
+        mask[layer] = sub_mask
     return mask
+
+def apply_mask(params, mask):
+    pruned_params = {}
+    for layer, param_dict in params.items():
+        layer_params = {}
+        for k, v in param_dict.items():
+            if 'w' in layer_name:
+                layer_param[k] = v * mask[layer][k]
+            else:
+                layer_param[k] = v
+        pruned_params[layer] = layer_param
+    return pruned_params
 
 
 
@@ -270,12 +300,8 @@ def main():
         prune_ratio = 0.5  # 50% of the weights will be pruned
         pruned_params = prune_weights_random_layer(params, prune_ratio)
 
-        # Using masking as a form of pruning
-        mask = create_mask(params, prune_ratio) # Create a mask
-        pruned_params = {k: v * mask[k] for k, v in params.items()} # Apply the mask
-
         jnp.save('./transfer/'+ str(i) + '/epoch_20_pruned.npy', pruned_params)
 
 
 
-main()
+#main()